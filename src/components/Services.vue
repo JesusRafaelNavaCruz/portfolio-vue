77ffe1f92ab5184b546aabda7ee17e6c72824bb1
Jesus Nava <jrnc9802@gmail.com>
2021-07-25 00:01:18 -0500

    Termina seccion Habilidades y Servicios

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,7 +1,129 @@
 <template>
   <section id="services">
     <v-container class="py-10 my-10">
-      
+      <v-row align="center" justify="center">
+        <v-col cols="10">
+          <v-row align="center" justify="center">
+            <v-col cols="10">
+              <h1 class="display-2 font-weight-bold mb-4 mt-10 text-center">
+                Mis servicios
+              </h1>
+              <h1 class="font-weight-light mb-2 text-center">
+                Lorem ipsum dolor sit amet.
+              </h1>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+
+      <v-row align="center" justify="center">
+        <v-col cols="10">
+          <v-row align="center" justify="center">
+            <v-col cols="10">
+              <v-row>
+                <v-col>
+                  <v-card flat>
+                    <v-card-title>
+                      <v-img src="@/assets/icons/ux.svg" max-width="80"></v-img>
+                    </v-card-title>
+                    <v-card-text>
+                      <h1 class="font-weight-bold text-uppercase mb-3">
+                        UX/UI
+                      </h1>
+                      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                      Temporibus, perferendis.</v-card-text
+                    >
+                  </v-card>
+                </v-col>
+                <v-col>
+                  <v-card flat>
+                    <v-card-title
+                      ><v-img src="@/assets/icons/ui.svg" max-width="80"></v-img
+                    ></v-card-title>
+                    <v-card-text>
+                      <h1 class="font-weight-bold text-uppercase mb-3">
+                        Diseño Responsivo
+                      </h1>
+                      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                      Temporibus, perferendis.</v-card-text
+                    >
+                  </v-card>
+                </v-col>
+                <v-col>
+                  <v-card flat>
+                    <v-card-title
+                      ><v-img
+                        src="@/assets/icons/disposicion.svg"
+                        max-width="80"
+                      ></v-img
+                    ></v-card-title>
+                    <v-card-text>
+                      <h1 class="font-weight-bold text-uppercase mb-3">
+                        Desarrollo Web
+                      </h1>
+                      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                      Temporibus, perferendis.</v-card-text
+                    >
+                  </v-card>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <v-card flat>
+                    <v-card-title
+                      ><v-img
+                        src="@/assets/icons/aprobacion.svg"
+                        max-width="80"
+                      ></v-img
+                    ></v-card-title>
+                    <v-card-text>
+                      <h1 class="font-weight-bold text-uppercase mb-3">
+                        Marketing Digital
+                      </h1>
+                      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                      Temporibus, perferendis.</v-card-text
+                    >
+                  </v-card>
+                </v-col>
+                <v-col>
+                  <v-card flat>
+                    <v-card-title
+                      ><v-img
+                        src="@/assets/icons/navegacion.svg"
+                        max-width="80"
+                      ></v-img
+                    ></v-card-title>
+                    <v-card-text>
+                      <h1 class="font-weight-bold text-uppercase mb-3">
+                        Desarrollo de software
+                      </h1>
+                      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                      Temporibus, perferendis.</v-card-text
+                    >
+                  </v-card>
+                </v-col>
+                <v-col>
+                  <v-card flat>
+                    <v-card-title
+                      ><v-img
+                        src="@/assets/icons/telefono-inteligente.svg"
+                        max-width="80"
+                      ></v-img
+                    ></v-card-title>
+                    <v-card-text>
+                      <h1 class="font-weight-bold text-uppercase mb-3">
+                        Aplicacion moviles
+                      </h1>
+                      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                      Temporibus, perferendis.</v-card-text
+                    >
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
     </v-container>
   </section>
 </template>
