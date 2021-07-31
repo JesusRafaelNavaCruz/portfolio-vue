1ae50a45e4b8c196ce9b2f3788747dba6163778d
Jesus Rafael Nava Cruz <jrnc9802@gmail.com>
2021-07-31 13:28:01 -0500

    Corrige color botton Contactame

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,7 +8,7 @@
               <h1 class="font-weight-regular text-uppercase mb-3">Hola</h1>
               <h1 class="display-2 font-weight-bold mb-4">Soy Jesus Nava</h1>
               <h1 class="font-weight-light mb-4">Desarrollador Web Front-End</h1>
-              <v-btn dark large @click="$vuetify.goTo('#contact')">Contactame</v-btn>
+              <v-btn dark large @click="$vuetify.goTo('#contact')" color="#2a3947">Contactame</v-btn>
             </v-col>
             <v-col md="6">
               <v-img src="@/assets/fotoPrincipal.png"></v-img>
